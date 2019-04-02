9ddfbb3eafd8048e474385734ba3053f7334d37f
socamporomani <socamporomani@nautilus10.danielcastelao.org>
2019-04-02 12:08:24 +0200

    fin

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin26;
+
+/**
+ *
+ * @author socamporomani
+ */
+public class Boletin26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Interfaz ini =new Interfaz();
+        ini.setVisible(true);
+    }
+    
+}
